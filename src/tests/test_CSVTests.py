874f798c2ec98e4ae617b0cfca30e306d5d48c61
jaylakhani14 <jal98@njit.edu>
2021-02-21 23:04:38 -0500

    Feature : Added coveralls and travis

--- src/tests/test_CSVTests.py
+++ src/tests/test_CSVTests.py
@@ -3,19 +3,21 @@ from src.src.CsvReader import CsvReader, ClassFactory
 from src.calculator.calculator import Calculator
 from pprint import pprint
 
-class MyTestCase(unittest.TestCase):
+class CSVTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.csv_reader = CsvReader('Tests/Data/employee_birthday.csv')
+        self.calculator = Calculator()
 
-    def test_return_data_as_objects(self):
-        people = self.csv_reader.return_data_as_class('person')
-        self.assertIsInstance(people, list)
-        test_class = ClassFactory('person', self.csv_reader.data[0])
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
 
-        for person in people:
-            self.assertEqual(person.__name__, test_class.__name__)
-            pprint(vars(people))
+    def test_result_is_zero_calculator(self):
+        self.assertEqual(self.calculator.result, 0)
 
+    def test_add_method_calculator(self):
+        calculator = Calculator()
+        test_data = CsvReader('/src/csv/addition.csv').data
+        self.assertEqual(calculator.add(row['Value 1'], row['Value 2']), float(row['Result']))
+        self.assertEqual(calculator.result, float(row['Result']))
 if __name__ == '__main__':
     unittest.main()
