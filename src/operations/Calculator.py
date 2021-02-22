fe7d79b5888b937cbecdfac5b59ff67149f080fa
jaylakhani14 <jal98@njit.edu>
2021-02-22 09:00:15 -0500

    Feature : Calculator.py

--- src/operations/Calculator.py
+++ src/operations/Calculator.py
@@ -25,26 +25,7 @@ class Calculator:
         self.result = square_root(a)
         return self.result
 
-    def _init_(self):
-        pass
 
-    def addition(a, b):
-        return a + b
-
-    def subtraction(a, b):
-        return a - b
-
-    def multiply(a, b):
-        return a * b
-
-    def division(a, b):
-        return a / b
-
-    def square(a):
-        return a ** 2
-
-    def square_root(a):
-        return a ** 0.5
 
 
 
