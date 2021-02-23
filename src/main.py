5d9ff11510f6d6e92dc07d2936a10960318eb5f7
jaylakhani14 <jal98@njit.edu>
2021-02-23 17:19:47 -0500

    Feature : code

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from src.calculator.calculator import Calculator
+
+
+def calculate(a, b):
+    # Use a breakpoint in the code line below to debug your script.
+    calc = Calculator()
+    print('done')
+
+
+
+if __name__ == '__main__':
+    calculate(1, 2)
+
