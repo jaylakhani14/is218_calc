592c7883c12a37e18fc74cfff0b7c4f15d33cdbb
jaylakhani14 <jal98@njit.edu>
2021-02-23 00:25:09 -0500

    Feature : code

--- src/Calculator.py
+++ src/Calculator.py
@@ -5,36 +5,32 @@ from src.operations.division import division
 from src.operations.square import square
 from src.operations.squareroot import square_root
 
-
-
 class Calculator:
     result = 0
 
-    def __add__(self, a, b):
-        self.result = addition(a,b)
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
         return self.result
 
-    def __sub__(self, a, b):
-        self.result = subtraction(a,b)
+    def sub(self, a, b):
+        self.result = subtraction(a, b)
         return self.result
 
-    def multiply(self , a ,b):
-        self.result = multiplication(a,b)
+    def mult(self, a, b):
+        self.result = multiplication(a, b)
         return self.result
 
-    def div(self, a, b):
+    def divi(self, a, b):
         self.result = division(a, b)
         return self.result
 
-    def square(self, a):
+    def squ(self, a):
         self.result = square(a)
         return self.result
 
-    def squareroot(self, a):
+    def root(self, a):
         self.result = square_root(a)
         return self.result
-
-
-
-
-
